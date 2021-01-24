e75bcbde768d31c6f0723691c38c7062a9c1ceb0
khaledrefaat <khaledrefaat08@gmail.com>
2021-01-24 22:10:01 +0200

    fixing little typo

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // figure out what set of credentials to return
 if (process.NODE_ENV === 'production') {
     // return the prod set of keys
-    module.exporst = require('./prod');
+    module.exports = require('./prod');
 } else {
     // return the dev set of keys
     module.exports = require('./dev');
